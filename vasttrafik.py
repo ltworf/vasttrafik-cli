6a6a94e6c193349b019c641110a31443404bc275
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2012-09-11 15:20:10 +0200

    ehm, corrected date

--- vasttrafik.py
+++ vasttrafik.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # coding=UTF-8
 # pysttrafik
-# Copyright (C) 2010  Salvo "LtWorf" Tomaselli
+# Copyright (C) 2012 Salvo "LtWorf" Tomaselli
 # 
 # pysttrafik is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
