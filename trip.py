90bb95b64d08cc7abdafca5ad70ef0503fd97f73
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2012-09-12 18:14:32 +0200

    stubby cli interface

--- trip.py
+++ trip.py
@@ -38,30 +38,32 @@ readline.parse_and_bind('tab: complete')
 readline.parse_and_bind('set editing-mode emacs')
 readline.set_completer_delims(" ")
 
-stops=[]
 
+
+def get_stop(line):
+    stops=[]
+    stops = vast.location(line)
+    
+    for i in range(len(stops)):
+        print "%d: %s" % (i,stops[i].name)
+        if i>8:
+            break
+    
+    while True:
+        line = raw_input('> ')
+        try:
+            return stops[int(line)]
+        except:
+            pass
+    
+    
+    
 while True:
-    line = raw_input('> ')
-    if len(line)==0:
-        sys.exit(0)
-    elif not line.isdigit():
-        stops = vast.location(line)
-        for i in range(len(stops)):
-            print "%d: %s" % (i,stops[i].name)
-            if i>8:
-                break
-    else:
-        trams = vast.board(stops[int(line)].id,time_span=120,departures=4)
-        prev_track=None
-        
-        f= open('/tmp/t.html','w')
-        f.write(gen_timetable_html(trams,vast.datetime_obj))
-        f.close()
-        print "\n\n\n\n\n\n\n\n\n"
-        print "\t\t%s, Time: %s\n" % (stops[int(line)].name,vast.datetime_obj)
-        for i in trams:
-            
-            if prev_track != i.track:
-                print "   == Track %s ==" % i.track
-            prev_track = i.track
-            print i.toTerm(vast.datetime_obj)
+    line = raw_input('FROM: > ')
+    orig = get_stop(line)
+    
+    line = raw_input('TO: > ')
+    dest = get_stop(line)
+    
+    print vast.trip(originId = orig.id, destId = dest.id)
+    
