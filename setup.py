6c122ce5015d73d397ce967716237a72b1057ccc
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2013-01-26 18:04:01 +0100

    setup for the debian package

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 from distutils.core import setup
 
-setup(name='pysttrafik',
-      version='0.1',
-      description='python API for Göteborg Västtrafik public API',
-      author='Salvo \'LtWorf\' Tomaselli',
-      author_email='tiposchi@tiscali.it',
-      url='https://github.com/ltworf/pysttrafik',
-      py_modules=['pysttrafik'],
-     )
+import glob
+
+setup(name="PyVasttrafik",
+      version="0.1",
+      author="Salvo 'LtWorf' Tomaselli",
+      author_email="tiposchi@tiscali.it",
+      url="https://github.com/ltworf/pysttrafik",
+      license="GNU General Public License Version 3 (GPLv3)",
+      scripts=['bin/pyvasttrafik'],
+      py_modules = ['gui', 'key', 'main', 'pysttrafik', 'selgui', 'stop_selector'],
+      data_files=[
+          ('icons', glob.glob('icons/*.png')),
+          ('/usr/share/applications', ['bin/pyvasttrafik.desktop']),
+      ],
+)
