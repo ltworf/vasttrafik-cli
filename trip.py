ec3055cb3b32de3a3df457396c6ea6b10ba9d619
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2017-02-22 22:04:31 +0100

    Update copyright

--- trip.py
+++ trip.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # coding=UTF-8
 # pysttrafik
-# Copyright (C) 2012 Salvo "LtWorf" Tomaselli
+# Copyright (C) 2012-2017 Salvo "LtWorf" Tomaselli
 #
 # pysttrafik is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
