ee0685b9d06a4cc85d744a90a7997d812f93b155
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2017-02-22 21:31:04 +0100

    Simplify stops by reusing the get_stop from trip

--- stops.py
+++ stops.py
@@ -19,9 +19,11 @@
 #
 # author Salvo "LtWorf" Tomaselli <tiposchi@tiscali.it>
 
-from pysttrafik import Vasttrafik, gen_timetable_html, get_key
 import readline
 import sys
+
+from pysttrafik import Vasttrafik, gen_timetable_html, get_key
+import trip
 
 key = get_key()
 if key == None:
@@ -34,35 +36,24 @@ readline.parse_and_bind('tab: complete')
 readline.parse_and_bind('set editing-mode emacs')
 readline.set_completer_delims(" ")
 
-stops = []
-
 while True:
-    line = input('> ')
-    if len(line) == 0:
-        sys.exit(0)
-    elif not line.isdigit():
-        stops = vast.location(line)
-        for i in range(len(stops)):
-            print("%d: %s" % (i, stops[i].name))
-            if i > 8:
-                break
-    else:
-        try:
-            trams = vast.board(
-                stops[int(line)].id, time_span=120, departures=4)
-        except:
-            print("Error")
-            sys.exit(1)
-        prev_track = None
 
-        f = open('/tmp/t.html', 'w')
-        f.write(gen_timetable_html(trams, vast.datetime_obj))
-        f.close()
-        print("\n\n\n\n\n\n\n\n\n")
-        print("\t\t%s, Time: %s\n" % (stops[int(line)].name, vast.datetime_obj))
-        for i in trams:
+    preset = sys.argv[1] if len(sys.argv) == 2 else ''
+    stop = trip.get_stop('> ', preset)
+    try:
+        trams = vast.board(stop.id, time_span=120, departures=4)
+    except:
+        print("Error")
+        sys.exit(1)
 
-            if prev_track != i.track:
-                print("   == Track %s ==" % i.track)
-            prev_track = i.track
-            print(i.toTerm(vast.datetime_obj))
+    f = open('/tmp/t.html', 'w')
+    f.write(gen_timetable_html(trams, vast.datetime_obj))
+    f.close()
+    print("\n\n\n\n\n\n\n\n\n")
+    print("\t\t%s, Time: %s\n" % (stop.name, vast.datetime_obj))
+    prev_track = None
+    for i in trams:
+        if prev_track != i.track:
+            print("   == Track %s ==" % i.track)
+        prev_track = i.track
+        print(i.toTerm(vast.datetime_obj))
